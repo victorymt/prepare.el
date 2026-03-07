a7184c7f44cf13631c1fc72bbecc2cc8910900ab
CV <victorCV@foxmail.com>
2026-03-07 11:55:24 +0800

    有了替代方案的 idea

--- prepare.el
+++ prepare.el
@@ -4,7 +4,21 @@
 ;; C-r: 进入只读模式
 ;; n/p
 ;; RET : 跳转
+;; R : 类似 dired 的 rename
+;; A : 添加一个文件路径
+;; d : 标记要删除的文件路径
+;; x : 删除标记的文件路径
+;; u : 删除标记
 
+;; Bugs or Todo: 或许我应该像 dired 那样分出一个 wdired
+;; C-c C-c : 从 wprepare mode -> prepare mode
+;; w       : 从 prepare mode -> wprepare mode
+;; 但似乎也不太对， wprepare 应该是想文本一样编辑， 而 prepare mode 则是必须通过命令编辑，根据实际的需求，我似乎没有 wprepare mode 的需求。
+;; 所以这是快捷键的设置不到位的缘故。
+
+;; Idea:
+;; 话说，我能不能直接 将 .prepare 设置为一个目录，要准备的文件直接链接在里面，然后我就可以用 dired 了
+;; md 我觉得这真是个好主意，但我决定先实现当前的项目，就当熟悉 elisp 编程了。这个主意以后再实现吧。
 (defvar prepare-buf-name "*prepare*")
 (defvar prepare-file "~/.emacs.d/.prepare")
 (defvar prepare/keymap (make-sparse-keymap))
